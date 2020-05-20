7ef25097412bc37e9495c82e0f0a7de6dd1ee29e
yuriyberezskyy <bereza95@gmail.com>
2020-05-19 22:23:03 -0400

    auth guard with redirection in blog page. if auth is false you can't reach CREATE A NEW POST

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 // import axios from 'axios';
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import Posts from "./Posts/Posts";
 import FullPost from "./FullPost/FullPost";
 import NewPost from "./NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
+  state = {
+    auth: false,
+  };
   render() {
     return (
       <div className="Blog">
@@ -42,7 +45,11 @@ class Blog extends Component {
         </header>
         <Switch>
           <Route path="/" exact component={Posts} />
-          <Route path="/new-post" exact component={NewPost} />
+          {this.state.auth ? (
+            <Route path="/new-post" exact component={NewPost} />
+          ) : (
+            <Redirect to="/" />
+          )}
           <Route path="/:id" exact component={FullPost} />
         </Switch>
       </div>
